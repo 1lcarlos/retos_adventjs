d17b51221cf77a052d6243e08a15749bb156943a
Luis Carlos <luiscardozoicg@gmail.com>
2022-12-12 11:26:53 -0500

    Reto 2 solucionado Advenjs Midu

--- reto2/reto2.js
+++ reto2/reto2.js
@@ -1,21 +1,20 @@
 const year = 2022;
-const holidays = ["01/06", "04/01", "12/25", "12/24"];
-const holiday = "12/25";
+const holidays = ["01/06", "04/01", "12/25", "12/24", "12/23"];
 
 function countHours(year, holidays) {
-  return holidays.map((holiday) => {
+  let horas = 0;
+  holidays.forEach((holiday) => {
     const fecha = new Date(`${holiday}/${year}`);
-    console.log(fecha);
     const dia = fecha.getDay();
     if (dia > 0 && dia < 6) {
-      return 2;
+      horas += 2;
+      return horas;
     } else {
       return 0;
     }
   });
+  return horas;
 }
 
-const dia = countHours(year, holidays);
-console.log(dia);
-/* const fecha =  new Date('12/25/2022')
-console.log(fecha.getDay()); */
+const di = countHours(year, holidays);
+console.log(di);
