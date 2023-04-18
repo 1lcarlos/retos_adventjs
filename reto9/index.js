131e24c3489dc11e775643f33bd11bf894035402
lcarlos <luiscardozoicg@gmail.com>
2023-04-18 11:09:07 -0500

    Reto completado

--- reto9/index.js
+++ reto9/index.js
@@ -1,1 +1,14 @@
 console.log("comienzo");
+
+const leds = [0, 0, 1, 0, 0];
+
+const countTime = leds => {
+  const ledStripsOff = leds.join('').split('1');
+  ledStripsOff[0] += ledStripsOff.pop();
+
+  return ledStripsOff.reduce((time, ledStripOff) => {
+    return Math.max(time, ledStripOff.length * 7);
+  }, 0);
+};
+
+console.log(countTime(leds));
